5c912bf6bc16c6b9d1b1b3aea451bf0874bfdab6
Chipsetx86 <chipsetx86@gmail.com>
2022-09-07 00:55:30 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Start application" << std::endl;
+
+    std::cout << "Close application" << std::endl;
+    return EXIT_SUCCESS;
+}
